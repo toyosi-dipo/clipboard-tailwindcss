5bb8e86688ca8ca0bd327c4ca2ef26f6023b3dd5
Oluwatoyosi <oladipooluwatoyosi@yahoo.com>
2023-04-03 21:15:43 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./**/*.{html, js}"],
+  theme: {
+    screen: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        strongCyan: "hsl(171, 66%, 46%)",
+        lightBlue: "hsl(233, 100%, 69%)",
+        darkGrayishBlue: "hsl(210, 10%, 33%)",
+        grayishBlue: "hsl(201, 11%, 66%)",
+      },
+      fontFamily: ["Bai Jamjuree", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
